08ea6bed431f0aa0b22634dbc29d08b996cc9687
Krzysztof <programowanie.krzysiek@gmail.com>
2019-10-24 14:34:29 +0200

    v1

---
+++ src/main/java/com/app/controller/PersonController.java
@@ -0,0 +1,21 @@
+package com.app.controller;
+
+import com.app.model.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/people")
+public class PersonController {
+    @GetMapping
+    public List<Person> getAll() {
+        return Arrays.asList(
+                Person.builder().name("A").age(11).build(),
+                Person.builder().name("B").age(12).build(),
+                Person.builder().name("C").age(13).build() );
+    }
+}
